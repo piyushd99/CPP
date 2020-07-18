53f409d16c51578eb063f27f0b772b5691a89340
piyushd99 <66412414+piyushd99@users.noreply.github.com>
2020-07-18 23:29:51 +0530

    Create Max Sum Contiguous Subarray.cpp

---
+++ Max Sum Contiguous Subarray.cpp
@@ -0,0 +1,31 @@
+int Solution::maxSubArray(const vector<int> &A) {
+
+    //initialise variables
+    int sum = 0, max = 0, min, flag = 0, j = 0;
+       
+    for(int i = 0; i < A.size(); i++){
+        sum = sum + A[i];                       //check sum of consecutive elements
+        if(sum < 0){
+            sum = 0;
+        }
+        if(sum > max){                          //replace the max by max sum
+            max = sum;
+        }
+        if(A[i] < 0){
+            j++;
+            if(j == 1){
+                min = A[i];
+            }
+            if(A[i] > min){
+                min = A[i];
+            }
+            flag++;
+        }
+    }
+    
+    if(flag == A.size()){
+        max = min; 
+    }
+    
+    return max;
+}
